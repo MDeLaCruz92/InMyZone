1652dd8710ead1f370b8a815906a5c5cef3a6246
MDeLaCruz92 <delacruzm10@gmail.com>
2016-12-28 18:53:12 -0500

    Initial Commit

---
+++ InMyZone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  InMyZone
+//
+//  Created by Michael De La Cruz on 12/28/16.
+//  Copyright © 2016 Michael De La Cruz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
